ca78754ddc5fcccd816be1d3b87d706eccb9a367
rafischer1 <artiefischer@gmail.com>
2022-11-08 11:54:17 -0500

    data types

---
+++ basics/data_types.py
@@ -0,0 +1,16 @@
+# Data Types
+
+# Fundamental Types
+
+# int
+# float
+# bool
+# str
+# list
+# tuple
+# set
+# dict
+
+# Classes -> custom types
+
+# Specialized Types (classes and modules from libraries)
