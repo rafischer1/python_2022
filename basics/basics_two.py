a4e3211dc9de44c9f47af76c15096391d46d03fa
rafischer1 <artiefischer@gmail.com>
2022-11-09 11:45:45 -0500

    short circuiting

--- basics/basics_two.py
+++ basics/basics_two.py
@@ -1,6 +1,6 @@
 # Conditional Logic
 is_old = True
-is_licensed = False
+is_licensed = True
 
 if is_old and is_licensed:
     print("Old enough and licensed!")
@@ -8,3 +8,13 @@ elif not is_licensed & is_old:
     print("No license")
 else:
     print("No way")
+
+# Ternary operator
+is_friend = False
+is_user = True
+can_message = "message allowed" if is_friend else "not allowed to message"
+print(can_message)
+
+# Short Circuiting 'or' 'and'
+print(is_friend or is_user, is_user and is_friend)
+# short circuits if the first condition is met
