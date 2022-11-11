5bed64cf6a371d6b6e6111454124b8547174712c
rafischer1 <artiefischer@gmail.com>
2022-11-10 20:46:30 -0500

    iterables

---
+++ basics/loops.py
@@ -0,0 +1,20 @@
+# For Loops
+my_list = ["a", "b", "c"]
+
+for i in my_list:
+    print(i, my_list.index(i))
+
+
+user = {"name": "Golem", "age": 543, "can_swim": False}
+
+# for i in user:
+#     print({i: user[i]})
+
+# for i in user.items():
+#     for x in user.values():
+#         print(i, x)
+
+for key, value in user.items():
+    print(key, value)
+
+# range()
