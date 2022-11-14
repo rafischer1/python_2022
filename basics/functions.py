e1eab909df6325bb90d481c047164d861bdcb408
rafischer1 <artiefischer@gmail.com>
2022-11-14 12:03:51 -0500

    default params and args

--- basics/functions.py
+++ basics/functions.py
@@ -1,6 +1,12 @@
 # Functions
-def say_hi():
-    print("hi")
+# paramatera define what arguments a function needs
+def print_message(msg="hi") -> None:
+    if not isinstance(msg, str):
+        print("Error: input must be string. Input type: ", type(msg))
+    else:
+        print(msg)
 
 
-say_hi()
+print_message("3")  # standard
+print_message(msg="ok")  # defined arguments
+print_message()  # uses default param
