3e386c292ba567d3ccff7ad53cdf215df732db30
rafischer1 <artiefischer@gmail.com>
2022-11-14 19:41:44 -0500

    excessive fizz_buzz

--- basics/functions.py
+++ basics/functions.py
@@ -10,3 +10,21 @@ def print_message(msg="hi") -> None:
 print_message("3")  # standard
 print_message(msg="ok")  # defined arguments
 print_message()  # uses default param
+
+
+def fizz_buzz(x: int) -> list:
+    res = []
+    for i in range(x + 1):
+        if i == 0:
+            pass
+        elif i % 3 == 0 and i % 5 == 0:
+            res.append(str(i) + " FIZZ BUZZ")
+        elif i % 3 == 0 and i % 5 != 0:
+            res.append(str(i) + " FIZZ")
+        elif i % 5 == 0 and i % 3 != 0:
+            res.append(str(i) + " BUZZ")
+    return res
+
+
+fizz = fizz_buzz(100)
+print(fizz)
