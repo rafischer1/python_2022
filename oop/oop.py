b0c7164c4017a8a151ef6637605738319d95b72d
rafischer1 <artiefischer@gmail.com>
2022-11-15 20:14:02 -0500

    oop classmethods

--- oop/oop.py
+++ oop/oop.py
@@ -8,4 +8,32 @@ class BigObject:  # Class
 
 obj1 = BigObject()  # instantiate
 
-print(type(obj1))
+# print(type(obj1))
+
+
+class Character:
+    logged_in = True  # class object attribute
+
+    def __init__(self, name):
+        if self.logged_in:
+            self.name = name
+
+    def move_forward(self):
+        return f'{self.name} ->'
+
+    def move_backward(self):
+        return f'{self.name} <-'
+
+    @classmethod
+    def return_to_start(cls):
+        return f'{cls.name} at start.'
+
+    @staticmethod
+    def game_over():
+        return "Game Over"
+
+
+c1 = Character(name="Me")
+c2 = Character("You")
+print(c1.move_forward())
+print(c2.move_backward())
