850ba51f15b898d1e95780d1bbf5996ff33a5ea3
rafischer1 <artiefischer@gmail.com>
2022-11-16 10:56:54 -0500

    imports and packages

---
+++ basics/modules/modules.py
@@ -0,0 +1,6 @@
+# Modules
+import utility  # creates compiled version pycache
+from package.shopping_cart import buy  # importing from a package
+
+print(utility.divide(100, 10))
+print(buy("apple"))
