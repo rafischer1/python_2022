b74182c19b70f91d5a1063c8efaca1ff6a00ec4c
rafischer1 <artiefischer@gmail.com>
2022-11-16 21:27:55 -0500

    serving static js

--- web_dev/first_web_server/server.py
+++ web_dev/first_web_server/server.py
@@ -1,11 +1,12 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, url_for
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
     print("HELLO RUNNING")
+    url_for('static', filename='script.js')
     return render_template("./index.html")
 
 
