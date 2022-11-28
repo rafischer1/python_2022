e276f9655b2c9915a0c2c647af621e1da78a66b7
rafischer1 <artiefischer@gmail.com>
2022-11-28 18:36:23 -0500

    email

--- scripting/script.py
+++ scripting/script.py
@@ -1,6 +1,8 @@
 from PIL import Image, ImageFilter
 
 img = Image.open("./img/nov_22.jpeg")
-# filtered_img = img.filter(ImageFilter.BLUR)
+filtered_img = img.filter(ImageFilter.BLUR)
+convert_img = img.convert("L")
 print(img.size, img.format, img.mode)
-filtered_img.save("./img/blur.png", "png")
+# filtered_img.save("./img/blur.png", "png")
+convert_img.show()
