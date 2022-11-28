c4695548d671cab4507389c16919723b0c4a7fbe
rafischer1 <artiefischer@gmail.com>
2022-11-28 13:59:19 -0500

    blurring images

--- scripting/script.py
+++ scripting/script.py
@@ -1,3 +1,6 @@
-from PIL import Image
+from PIL import Image, ImageFilter
 
-img = Image.open()
+img = Image.open("./img/nov_22.jpeg")
+# filtered_img = img.filter(ImageFilter.BLUR)
+print(img.size, img.format, img.mode)
+filtered_img.save("./img/blur.png", "png")
