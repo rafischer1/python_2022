4b7a864e57b949c5de7a7935fea24a00e4f58dac
rafischer1 <artiefischer@gmail.com>
2022-11-30 14:52:45 -0500

    world cup notifications

--- scripting/push_notifications.py
+++ scripting/push_notifications.py
@@ -2,16 +2,17 @@ import requests
 import sys
 
 
-def send_message(message, priority):
-    url = "https://ntfy.sh/raf-py"
+def send_message(message):
+    url = "https://ntfy.sh/world_cup"
+    priority = "4"
     requests.post(url, data=message.encode(encoding='utf-8'),
-                  headers={"Priority": priority, "Tags": 'strawberry', "topic": "python test".upper()})
+                  headers={"Priority": priority, "Tags": 'skull', "topic": "python test".upper()})
 
 
-def main(message, priority):
-    send_message(message, priority)
+def main(message):
+    send_message(message)
     return '(done)'
 
 
 if __name__ == "__main__":
-    sys.exit(main(sys.argv[1], sys.argv[2]))
+    sys.exit(main(sys.argv[1]))
