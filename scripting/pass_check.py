8bc06d8b80ff38e110162f2be37f7d969c973776
rafischer1 <artiefischer@gmail.com>
2022-11-30 11:24:22 -0500

    password checker up and running

--- scripting/pass_check.py
+++ scripting/pass_check.py
@@ -1,5 +1,6 @@
 import requests
 import hashlib
+import sys
 
 
 def request_api_data(query_char):
@@ -10,6 +11,29 @@ def request_api_data(query_char):
     return res
 
 
+def get_password_leaks_count(hashes, hash_to_check):
+    hashes = (line.split(":") for line in hashes.text.splitlines())
+    for h, count in hashes:
+        if h == hash_to_check:
+            return count
+    return 0
+
+
 def pwned_api_check(password):
-    sha1_hash_password = hashlib.sha1(password.encode('utf-8'))
-    # check api res for password
+    sha1_hash_password = hashlib.sha1(
+        password.encode('utf-8')).hexdigest().upper()
+    first_five_char, tail = sha1_hash_password[:5], sha1_hash_password[5:]
+    response = request_api_data(first_five_char)
+    return get_password_leaks_count(response, tail)
+
+
+def main(args):
+    for password in args:
+        count = pwned_api_check(password)
+        if count:
+            print(f'{password} was found {count} times...change that password')
+        else:
+            print(f'{password} not found')
+
+
+main(sys.argv[1:])
