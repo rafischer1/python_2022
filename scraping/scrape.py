5455ad2790d1bd5da2fe444d4dde10203ace9c42
rafischer1 <artiefischer@gmail.com>
2022-12-06 10:14:46 -0500

    soup

--- scraping/scrape.py
+++ scraping/scrape.py
@@ -1,1 +1,8 @@
 # Web Scarper using api
+import requests
+from bs4 import BeautifulSoup
+
+res = requests.get("https://news.ycombinator.com/news")
+
+soup = BeautifulSoup(res.text, "html.parser")
+print(soup.title)
---
+++ scraping/scraping.md
@@ -0,0 +1,5 @@
+# Web Scraper
+
+[Beautiful Soup](https://www.crummy.com/software/BeautifulSoup/bs4/doc/)
+
+[Requests](https://pypi.org/project/requests/)
