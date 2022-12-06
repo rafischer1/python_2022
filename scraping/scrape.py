4ed09c379f673b1f4100e1268a550caedcc7f065
rafischer1 <artiefischer@gmail.com>
2022-12-06 11:10:13 -0500

    Scraper scraping

--- scraping/scrape.py
+++ scraping/scrape.py
@@ -5,4 +5,18 @@ from bs4 import BeautifulSoup
 res = requests.get("https://news.ycombinator.com/news")
 
 soup = BeautifulSoup(res.text, "html.parser")
-print(soup.title)
+links = soup.select(".sitestr")
+votes = soup.select(".score")
+
+
+def create_custom_hn(links, votes):
+    hn = []
+    for i, item in enumerate(links):
+        title = links[i].getText()
+        href = links[i].get("href", None)
+        hn.append({"title": title, "href": href})
+    return hn
+
+
+new_hn = create_custom_hn(links, votes)
+print(new_hn)
