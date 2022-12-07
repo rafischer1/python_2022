8407aaddf3efa70fd1a8cff08b49c75938f9d9b0
rafischer1 <artiefischer@gmail.com>
2022-12-07 10:01:11 -0500

    feat: web scraper basic

--- scraping/scrape.py
+++ scraping/scrape.py
@@ -6,17 +6,22 @@ res = requests.get("https://news.ycombinator.com/news")
 
 soup = BeautifulSoup(res.text, "html.parser")
 links = soup.select(".sitestr")
-votes = soup.select(".score")
+subtext = soup.select(".subtext")
 
 
-def create_custom_hn(links, votes):
+def create_custom_hn(links, subtext):
     hn = []
     for i, item in enumerate(links):
         title = links[i].getText()
+        vote = subtext[i].select(".score")
         href = links[i].get("href", None)
-        hn.append({"title": title, "href": href})
+        if len(vote):
+            hn.append(
+                {"title": title, "score": vote[0].getText(), "href": href})
+        else:
+            hn.append({"title": title, "score": None, "href": href})
     return hn
 
 
-new_hn = create_custom_hn(links, votes)
+new_hn = create_custom_hn(links, subtext)
 print(new_hn)
