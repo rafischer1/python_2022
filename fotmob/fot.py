5960e279f8f06b00c11528dbfd98dd43dca4ff00
rafischer1 <artiefischer@gmail.com>
2022-12-30 09:18:47 -0500

    building fot results

--- fotmob/fot.py
+++ fotmob/fot.py
@@ -2,6 +2,8 @@ from mobfot import MobFot
 client = MobFot()
 # res = client.get_matches_by_date("20221230")
 res = client.get_league(id=87)
-print(f"fotmob???: {res}")
+table = res['table']
+print(f"fotmob???: {table[0]}")
 
 # la liga id: 87
+# ['tabs', 'details', 'seostr', 'QAData', 'table', 'transfers', 'news', 'overview', 'stats', 'matches', 'playoff']
